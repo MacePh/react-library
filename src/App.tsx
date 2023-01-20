fc4775d0e67f660f27a58981f447b2ea5da0ffd4
MacePh <115651516+MacePh@users.noreply.github.com>
2023-01-19 23:14:20 -0500

    up to footer. Lecture 69

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,20 @@ import React from 'react';
 import './App.css';
 import { Carousel } from './layouts/HomePage/Carousel';
 import { ExploreTopBooks } from './layouts/HomePage/ExploreTopBooks';
+import { Heros } from './layouts/HomePage/Heros';
+import { LibraryServices } from './layouts/HomePage/LibraryServices';
+import { Footer } from './layouts/NavbarAndFooter/Footer';
 import { Navbar } from './layouts/NavbarAndFooter/Navbar';
 
 function App() {
   return (
     <div>
-    <Navbar />
+      <Navbar />
       <ExploreTopBooks />
       <Carousel />
+      <Heros />
+      <LibraryServices />
+      <Footer />
     </div>
   )
 }
